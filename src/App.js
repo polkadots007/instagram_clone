1fb42867567090f26d938a4a85b8082c28cdb251
1950019 <priyansiparida@gmail.com>
2023-02-19 19:20:30 +0530

    revert changes

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
   const UserContextProp = useMemo(() => ({ user }), [user]);
   return (
     <UserContext.Provider value={UserContextProp}>
-      <Router basename="/instagram_clone">
+      <Router>
         <Suspense
           fallback={
             <p
