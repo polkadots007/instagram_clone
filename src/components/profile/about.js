dc6b5c10283c50435bb7a13e2696438d77faf8c2
1950019 <priyansiparida@gmail.com>
2023-03-26 18:54:37 +0530

    added emoji picker

--- src/components/profile/about.js
+++ src/components/profile/about.js
@@ -1,5 +1,5 @@
-/* eslint-disable react/jsx-no-useless-fragment */
 import { useState, useEffect } from 'react';
+import Picker from 'emoji-picker-react';
 import PropTypes from 'prop-types';
 import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
@@ -22,6 +22,7 @@ export default function About({ fullScreen, open, handleClose, profileUserName, 
   });
   const [error, setError] = useState(null);
   const [isSaveClicked, setSaveClicked] = useState(false);
+  const [showEmojis, setShowEmojis] = useState(false);
   const reloadPage = true;
   const { user: userDetails } = useUser();
 
@@ -50,6 +51,14 @@ export default function About({ fullScreen, open, handleClose, profileUserName, 
     setProfileInfo((prevInfo) => ({ ...prevInfo, [fieldType]: target.value }));
   };
 
+  const addEmojiToText = (emojiObject, event) => {
+    setProfileInfo((prevInfo) => ({ ...prevInfo, bio: prevInfo.bio + emojiObject.emoji }));
+  };
+
+  const handleEmojis = () => {
+    setShowEmojis(!showEmojis);
+  };
+
   const handleCancel = () => {
     setProfileInfo({
       name: profile.fullName,
@@ -59,6 +68,7 @@ export default function About({ fullScreen, open, handleClose, profileUserName, 
     });
     setError(null);
     setSaveClicked(false);
+    setShowEmojis(false);
     handleClose();
   };
 
@@ -222,6 +232,22 @@ export default function About({ fullScreen, open, handleClose, profileUserName, 
                 value={profileInfo.bio}
                 onChange={(e) => handleChange(e, 'bio')}
               />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="w-6 h-6 mt-1 hover:cursor-pointer"
+                onClick={handleEmojis}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15.182 15.182a4.5 4.5 0 01-6.364 0M21 12a9 9 0 11-18 0 9 9 0 0118 0zM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75zm-.375 0h.008v.015h-.008V9.75zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75zm-.375 0h.008v.015h-.008V9.75z"
+                />
+              </svg>
+              {showEmojis && <Picker onEmojiClick={addEmojiToText} emojiStyle="native" />}
               <p className="text-gray-primary text-sm mt-1 ml-1">{profileInfo.bio.length}/150</p>
             </div>
           </div>
